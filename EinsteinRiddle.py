7be2ff71b26619412bc9bb0b685e1a29d89fdfc2
Juande Santander-Vela <Juande.Santander@ALMA.cl>
2022-12-11 15:48:22 -0300

    Remove initial version of constraints as comments

--- EinsteinRiddle.py
+++ EinsteinRiddle.py
@@ -39,25 +39,6 @@ problem.addConstraint(lambda d, h: abs(d-h)==1, ["Dunhill", "horses"])
 problem.addConstraint(lambda bm, b: bm==b, ["Blue Master", "beer"])
 problem.addConstraint(lambda g, p: g==p, ["German", "Prince"])
 problem.addConstraint(lambda n, b: abs(n-b)==1, ["Norwegian", "blue"])
-#facts = [
-##	(None, 'red', 'Brit', None, None, None, None, None),
-##	(None, None, 'Swede', None, 'dogs', None, None, None),
-##	(None, None, 'Dane', None, None, 'tea', None, None),
-##	(None, 'green', None, None, None, None, None, 'white'),
-##	(None, 'white', None, None, None, None, 'green', None),
-##	(None, 'green', None, None, None, 'coffee', None, None),
-##	(None, None, None, 'Pall Mall', 'birds', None, None, None),
-##	(None, 'yellow', None, 'Dunhill', None, None, None, None),
-##    (3, None, None, None, None, 'milk', 'colors:blue', None),
-##    (1, None, 'Norwegian', None, None, None, 0, 'colors:blue'),
-##    (None, None, None, 'Blends', None, None, 'pets:cats'), None), # This might not be true, but the next one
-##    #(None, None, None, 'Blends', None, None, None, 'pets:cats'), # This might not be true, but the previous one
-##    (None, None, None, 'Dunhill', None, None, 'pets:horses', None), # This might not be true, but the next one
-##    #(None, None, None, 'Dunhill', None, None, None, 'pets:horses'), # This might not be true, but the previous one
-##    (None, None, None, 'Blue Master', 'beer', None, None, None),
-##    (None, None, 'German', 'Prince', None, None, None, None),
-##    (2, 'blue', None, None, None, None, None, None),
-#]
 
 solution = problem.getSolutions()
 print(f"Solutions found: {len(solution)}")
